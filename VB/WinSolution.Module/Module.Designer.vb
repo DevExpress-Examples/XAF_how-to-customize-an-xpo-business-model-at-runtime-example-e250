513951fe657bf5a94f929778b0cc36a4644ce271
DevExpressExampleBot <fakeEmail>
2025-03-13 11:39:43 +0400

    Source auto update [skip ci]

--- VB/WinSolution.Module/Module.Designer.vb
+++ VB/WinSolution.Module/Module.Designer.vb
@@ -1,37 +1,37 @@
-﻿Namespace WinSolution.Module
-	Partial Public Class WinSolutionModule
-		''' <summary> 
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace WinSolution.[Module]
 
-		''' <summary> 
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class WinSolutionModule
 
-		#Region "Component Designer generated code"
+        ''' <summary> 
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary> 
-		''' Required method for Designer support - do not modify 
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			' 
-			' WinSolutionModule
-			' 
-			Me.AdditionalExportedTypes.Add(GetType(MyXPOClassLibrary.PersistentObject1))
-			Me.AdditionalExportedTypes.Add(GetType(MyXPOClassLibrary.PersistentObject2))
-			Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.SystemModule.SystemModule))
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
-	End Class
+#Region "Component Designer generated code"
+        ''' <summary> 
+        ''' Required method for Designer support - do not modify 
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            ' 
+            ' WinSolutionModule
+            ' 
+            Me.AdditionalExportedTypes.Add(GetType(MyXPOClassLibrary.PersistentObject1))
+            Me.AdditionalExportedTypes.Add(GetType(MyXPOClassLibrary.PersistentObject2))
+            Me.RequiredModuleTypes.Add(GetType(DevExpress.ExpressApp.SystemModule.SystemModule))
+        End Sub
+#End Region
+    End Class
 End Namespace
